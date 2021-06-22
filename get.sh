56c18e6a11e64418089b80dcbe4e79bffc2fd446
hkmk <>
2021-06-22 16:33:06 +0900

    modify wget options

--- get.sh
+++ get.sh
@@ -25,7 +25,7 @@ while :
 do
     for class in M7 A1 B1 B2 C1 C2;
     do
-	wget --load-cookies cookies.txt http://member.meijinsen.jp/pay/game_list/meijinsen/${year}/${class}/calendar.html -O tmp/${year}_${class}.html
+	wget -w 1 --random-wait --load-cookies cookies.txt http://member.meijinsen.jp/pay/game_list/meijinsen/${year}/${class}/calendar.html -O tmp/${year}_${class}.html
     done
     if [ ! "$?" -eq 0 ]; then
 	break
@@ -34,7 +34,7 @@ do
 done
 
 for class in M7 A1 B1 B2 C1 C2; do
-    wget --load-cookies cookies.txt http://member.meijinsen.jp/pay/game_list/meijinsen/latest/${class}/calendar.html -O tmp/latest_${class}.html
+    wget -w 1 --random-wait --load-cookies cookies.txt http://member.meijinsen.jp/pay/game_list/meijinsen/latest/${class}/calendar.html -O tmp/latest_${class}.html
 done
 
 for file in `ls tmp/*.html`;
@@ -47,11 +47,11 @@ rm tmp/tmp
 
 for file in `cat tmp/kiflist`;
 do
-    wget -q --load-cookies cookies.txt http://member.meijinsen.jp/${file} -O tmp/tmp;
+    wget -w 1 --random-wait -q --load-cookies cookies.txt http://member.meijinsen.jp/${file} -O tmp/tmp;
     filehead=`echo ${file} | sed "s/[0-9]*\.html//g"`
     for idx in `grep -o -E "[0-9]+.(txt|html)" tmp/tmp | cut -d '.' -f 1,1 | sort -u`;
     do
-	wget --load-cookies cookies.txt http://member.meijinsen.jp${filehead}${idx}.txt -P kif/
+	wget -w 1 --random-wait --load-cookies cookies.txt http://member.meijinsen.jp${filehead}${idx}.txt -P kif/
     done
 done
 
